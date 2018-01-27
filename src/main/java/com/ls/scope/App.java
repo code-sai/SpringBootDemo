e07745ee2ee7f3862236d84b5837af73e55e0ba0
lisai <893579440@qq.com>
2018-01-27 16:20:12 +0800

    commit

--- src/main/java/com/ls/scope/App.java
+++ src/main/java/com/ls/scope/App.java
@@ -12,6 +12,7 @@ public class App {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ScopeConfig.class);
 		DemoSingletonService s1 = context.getBean(DemoSingletonService.class);
 		DemoSingletonService s2 = context.getBean(DemoSingletonService.class);
+		
 		DemoPrototypeService p1 = context.getBean(DemoPrototypeService.class);
 		DemoPrototypeService p2 = context.getBean(DemoPrototypeService.class);
 		System.out.println("s1与s2是否相等：" + s1.equals(s2));
