05126dbb8f8b2f1a574316c52bb1b8f1425eb48b
lisai <893579440@qq.com>
2018-01-29 10:23:09 +0800

    修改注解

--- src/main/java/com/ls/di/FunctionService.java
+++ src/main/java/com/ls/di/FunctionService.java
@@ -1,6 +1,7 @@
 package com.ls.di;
 
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
+
 /**
  * @Service 注解声明当前FunctionService 类是Spring 管理的一个 Bean.
  * 其中使用 @Component @Service @Repository @Controller 是等效的，可以根据需要选用。
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Service;
  *
  * @date 2017年10月30日下午2:59:38
  */
-@Service
+@Component
 public class FunctionService {
 
 	public String sayHello(String word) {
